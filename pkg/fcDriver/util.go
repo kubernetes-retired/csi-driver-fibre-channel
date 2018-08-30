1eebc17fc5c77990b8658abfe26d263821c55bbe
Mathusan Selvarajah <mselvara@redhat.com>
2018-08-30 10:28:44 -0400

    Adding grpc server.go file

--- pkg/fcDriver/util.go
+++ pkg/fcDriver/util.go
@@ -2,11 +2,10 @@ package fc
 
 import (
 	"github.com/container-storage-interface/spec/lib/go/csi/v0"
-	"github.com/kubernetes-csi/drivers/pkg/csi-common"
 )
 
 func RunNodePublishServer(d *CSIDriver, ns csi.NodeServer, ids csi.IdentityServer) {
-	s := csicommon.NewNonBlockingGRPCServer()
+	s := NewNonBlockingGRPCServer()
 	s.Start(d.endpoint, ids, nil, ns)
 	s.Wait()
 }
